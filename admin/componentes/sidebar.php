d1d9ff55d1b0d1e927424407a8afdce5024f1658
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2023-08-01 13:51:21 +0100

    agregando el panel de administracion

---
+++ admin/componentes/sidebar.php
@@ -0,0 +1,25 @@
+ <!-- sidebar menu -->
+ <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+   <div class="menu_section">
+     <h3>General</h3>
+     <ul class="nav side-menu">
+       <li><a href="index.php"><i class="fa fa-home"></i> Inicio </a>
+       <li><a><i class="fa fa-tags"></i> Gestion <span class="fa fa-chevron-down"></span></a>
+         <ul class="nav child_menu">
+           <li><a href="./campus.php">Campus</a></li>
+           <li><a href="./carreras.php">Carrera</a></li>
+           <li><a href="./sede.php">Sede</a></li>
+           <li><a href="./facultad.php">Facultad</a></li>
+         </ul>
+       </li>
+       <li><a><i class="fa fa-newspaper-o"></i> Noticias <span class="fa fa-chevron-down"></span></a>
+         <ul class="nav child_menu">
+           <li><a href="./noticias.php">Nueva Noticia</a></li>
+           <li><a href="./categorias.php">Categorias</a></li>
+         </ul>
+       </li>
+       <li><a><i class="fa fa-calendar"></i>Eventos </a></li>
+     </ul>
+   </div>
+ </div>
+ <!-- /sidebar menu -->
